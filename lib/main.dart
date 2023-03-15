d5b8983c887390cd3dfef75290c6e59c41290fdc
sufiyan <sufiyansakkeer616@gmail.com>
2023-03-15 15:55:58 +0530

    firebase web also integrated

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,26 @@ import 'package:bsocial/view/layout/mobile_screen_layout.dart';
 import 'package:bsocial/view/layout/responsive_layout_building.dart';
 import 'package:bsocial/view/layout/web_screen_layout.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //* Initialize fire base
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCNm9bInJPhx4C-QMFDvGUPmGBcTZiqWQ4",
+        appId: "1:788358758157:web:52a33c61c5f95b56f8ba44",
+        messagingSenderId: "788358758157",
+        projectId: "bsocial-9e6c3",
+        storageBucket: "bsocial-9e6c3.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
