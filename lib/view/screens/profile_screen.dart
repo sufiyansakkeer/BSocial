b5d6528cb39c253287ae7a204dffdc3768557d1c
sufiyan <sufiyansakkeer616@gmail.com>
2023-03-30 12:34:19 +0530

    search field completed
    -- random images showing added in search field
    -- bugs fixed

--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bsocial/resources/auth_methods.dart';
+import 'package:bsocial/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -7,16 +8,26 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            AuthMethods().signOutUser(context);
-          },
-          child: const Text(
-            'sign out',
-          ),
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text(
+          "userName",
         ),
+        elevation: 0,
       ),
+      body: ListView(
+        children: [],
+      ),
+      // body: Center(
+      //   child: TextButton(
+      //     onPressed: () {
+      //       AuthMethods().signOutUser(context);
+      //     },
+      //     child: const Text(
+      //       'sign out',
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
--- lib/view/screens/search_screen.dart
+++ lib/view/screens/search_screen.dart
@@ -4,6 +4,7 @@ import 'package:bsocial/provider/search_provider.dart';
 import 'package:bsocial/utils/colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -50,15 +51,29 @@ class SearchScreen extends StatelessWidget {
                       builder: (BuildContext context, AsyncSnapshot snapshot) {
                         if (!snapshot.hasData) {
                           return const Center(
+                            child: Text("No data found"),
+                          );
+                        }
+                        if (snapshot.hasError) {
+                          return const Center(
+                            child:
+                                Text("Some error occurred while fetching data"),
+                          );
+                        }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
                             child: CircularProgressIndicator(),
                           );
                         }
                         return ListView.builder(
                           itemBuilder: (context, index) {
+                            log("${(snapshot.data)}");
                             return ListTile(
                               leading: CircleAvatar(
                                 backgroundImage: NetworkImage(
-                                    snapshot.data!.docs[index]["photoUrl"]),
+                                    (snapshot.data! as dynamic).docs[index]
+                                        ["photoUrl"]),
                               ),
                               title:
                                   Text(snapshot.data!.docs[index]["username"]),
@@ -68,7 +83,38 @@ class SearchScreen extends StatelessWidget {
                         );
                       },
                     )
-                  : Text("posts");
+                  : FutureBuilder(
+                      future:
+                          FirebaseFirestore.instance.collection("posts").get(),
+                      // initialData: InitialData,
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: Text(
+                              "Error while fetching Data",
+                            ),
+                          );
+                        }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return MasonryGridView.builder(
+                          mainAxisSpacing: 10,
+                          crossAxisSpacing: 10,
+                          gridDelegate:
+                              SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3),
+                          itemBuilder: (context, index) {
+                            return Image.network(
+                                snapshot.data.docs[index]["postUrl"]);
+                          },
+                          itemCount: snapshot.data.docs.length,
+                        );
+                      },
+                    );
             }),
           )),
     );
